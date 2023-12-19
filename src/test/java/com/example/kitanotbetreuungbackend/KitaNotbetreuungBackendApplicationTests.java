2af784519c692667f42ee22c39d3c613267e387f
ErbekB <145931150+ErbekB@users.noreply.github.com>
2023-12-19 09:50:44 +0100

    First Commit

---
+++ src/test/java/com/example/kitanotbetreuungbackend/KitaNotbetreuungBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitanotbetreuungbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitaNotbetreuungBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
