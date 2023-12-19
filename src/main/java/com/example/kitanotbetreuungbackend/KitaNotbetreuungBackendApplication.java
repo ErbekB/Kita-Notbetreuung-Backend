82a1499193f550b56e951b006e497e9b25becb96
ErbekB <145931150+ErbekB@users.noreply.github.com>
2023-12-19 09:57:28 +0100

    First Commit

--- src/main/java/com/example/kitanotbetreuungbackend/KitaNotbetreuungBackendApplication.java
+++ src/main/java/com/example/kitanotbetreuungbackend/KitaNotbetreuungBackendApplication.java
@@ -8,7 +8,6 @@ public class KitaNotbetreuungBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KitaNotbetreuungBackendApplication.class, args);
-        System.out.println("Hallo");
     }
 
 }
