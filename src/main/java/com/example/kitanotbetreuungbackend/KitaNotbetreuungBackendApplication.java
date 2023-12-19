4fc804ecca5653e5e0b53f294846f189d63bb47b
ErbekB <145931150+erbekb@users.noreply.github.com>
2023-12-19 11:57:59 +0100

    Dies ist ein PullRequestPush

--- src/main/java/com/example/kitanotbetreuungbackend/KitaNotbetreuungBackendApplication.java
+++ src/main/java/com/example/kitanotbetreuungbackend/KitaNotbetreuungBackendApplication.java
@@ -8,6 +8,7 @@ public class KitaNotbetreuungBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KitaNotbetreuungBackendApplication.class, args);
+        System.out.println("Hallo Push");
     }
 
 }
