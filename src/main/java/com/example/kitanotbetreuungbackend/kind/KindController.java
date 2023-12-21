172644c4ac5a6fafa0fb18256dd2493f2e05359e
JulianSoe <j.soe@gmx.de>
2023-12-21 14:07:28 +0100

    Notbetreuung Button

--- src/main/java/com/example/kitanotbetreuungbackend/kind/KindController.java
+++ src/main/java/com/example/kitanotbetreuungbackend/kind/KindController.java
@@ -1,16 +1,42 @@
 package com.example.kitanotbetreuungbackend.kind;
-import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+import com.example.kitanotbetreuungbackend.kita.KitaRepository;
+import com.example.kitanotbetreuungbackend.user.User;
+import com.example.kitanotbetreuungbackend.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class KindController {
-    @GetMapping("/notfall/{id}")
-    public KitaGruppeDTO uebersicht(@RequestParam long id){
-        return new KitaGruppeDTO();
+    private KindRepository kindRepository;
+    private KitaRepository kitaRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public KindController(KindRepository kindRepository, KitaRepository kitaRepository, UserRepository userRepository) {
+        this.kindRepository = kindRepository;
+        this.kitaRepository = kitaRepository;
+        this.userRepository = userRepository;
     }
+
+    @GetMapping("/notfall/{id}")
+    public KitaGruppeDTO uebersicht(@PathVariable long id) {
+        if (userRepository.existsById(id)){
+            User benutzer= userRepository.findById(id).get();
+            List<Kind> KinderListe = benutzer.getKind().get(0).getKitaGruppe().getKinder();
+            boolean teilnahme = benutzer.getKind().get(0).isTeilnahmeNotbetreuung();
+            return new KitaGruppeDTO(KinderListe, teilnahme);
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Benutzer nicht gefunden");
+    }
+
     @PostMapping("/notfall/{id}")
-    public StatusKindDTO bearbeitung(@RequestParam long id){
+    public StatusKindDTO bearbeitung(@RequestParam long id) {
         return new StatusKindDTO();
     }
 }
-
--- src/main/java/com/example/kitanotbetreuungbackend/kind/KitaGruppeDTO.java
+++ src/main/java/com/example/kitanotbetreuungbackend/kind/KitaGruppeDTO.java
@@ -1,4 +1,30 @@
 package com.example.kitanotbetreuungbackend.kind;
 
+import java.util.List;
+
 public class KitaGruppeDTO {
+    private List<Kind> kinder;
+
+    private boolean teilnahmeNotbetreuung;
+
+    public KitaGruppeDTO(List<Kind> kinder, boolean teilnahmeNotbetreuung) {
+        this.kinder = kinder;
+        this.teilnahmeNotbetreuung = teilnahmeNotbetreuung;
+    }
+
+    public List<Kind> getKinder() {
+        return kinder;
+    }
+
+    public void setKinder(List<Kind> kinder) {
+        this.kinder = kinder;
+    }
+
+    public boolean isTeilnahmeNotbetreuung() {
+        return teilnahmeNotbetreuung;
+    }
+
+    public void setTeilnahmeNotbetreuung(boolean teilnahmeNotbetreuung) {
+        this.teilnahmeNotbetreuung = teilnahmeNotbetreuung;
+    }
 }
