b629fd33c0d3caa69ebfb2eab187f63c94bc48c0
ErbekB <145931150+ErbekB@users.noreply.github.com>
2023-12-27 15:37:05 +0100

    Vorbereitung auf die Registrierungsfunktion

--- src/main/java/com/example/kitanotbetreuungbackend/kitaGruppe/KitaGruppeRepository.java
+++ src/main/java/com/example/kitanotbetreuungbackend/kitaGruppe/KitaGruppeRepository.java
@@ -2,5 +2,8 @@ package com.example.kitanotbetreuungbackend.kitaGruppe;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface KitaGruppeRepository extends JpaRepository<KitaGruppe, Long> {
+    Optional<KitaGruppe> findByName(String kitaGruppeName);
 }
